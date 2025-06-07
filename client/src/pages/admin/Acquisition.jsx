7711158e4ac52a7245dc67e59476167d01312596
hachisnail <jeffersontalagtag06@gmail.com>
2025-06-08 02:05:29 +0800

    added h breakpoint

--- client/src/pages/admin/Acquisition.jsx
+++ client/src/pages/admin/Acquisition.jsx
@@ -2,7 +2,8 @@ import Breadcrumb from "../../components/Breadcrumb"
 
 const Acquisition = () => {
   return (
-    <div className="w-full h-full max-w-[137rem] max-h-[69rem] 3xl:max-w-[175rem] 3xl:max-h-[88rem] overflow-scroll">
+    <div className="w-full h-full  max-w-[137rem] 1_5:max-h-[69rem] 2xl:max-h-[81rem] 3xl:max-w-[175rem] 3xl:max-h-[88rem] overflow-scroll">
+
 
 
 

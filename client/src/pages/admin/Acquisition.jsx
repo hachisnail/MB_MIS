e91e08d4ff12af105cbc0d2dafee9859d4577fcb
hachisnail <jeffersontalagtag06@gmail.com>
2025-06-08 01:33:53 +0800

    f***

--- client/src/pages/admin/Acquisition.jsx
+++ client/src/pages/admin/Acquisition.jsx
@@ -2,7 +2,8 @@ import Breadcrumb from "../../components/Breadcrumb"
 
 const Acquisition = () => {
   return (
-    <div className='w-[80rem] h-[41rem] bg-gray-300 overflow-scroll'>
+    <div className=' w-[137rem] h-[69rem] 3xl:w-[175rem] 3xl:h-[88rem] overflow-scroll'>
+
 
       
 

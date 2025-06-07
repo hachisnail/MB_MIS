802aeda4fc77ad493e163c4e775226738996d679
hachisnail <jeffersontalagtag06@gmail.com>
2025-06-08 02:09:08 +0800

    f**** 2

--- client/src/components/pages/viewArtifacts.jsx
+++ client/src/components/pages/viewArtifacts.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Breadcrumb from '../Breadcrumb'
 const viewArtifacts = () => {
   return (
-    <div className='w-[80rem] h-[41rem] overflow-scroll'>
+    <div className="w-full h-full  max-w-[137rem] 1_5:max-h-[69rem] 2xl:max-h-[81rem] 3xl:max-w-[175rem] 3xl:max-h-[88rem] overflow-scroll">
+
 
       
 

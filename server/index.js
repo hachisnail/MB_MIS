295fd977125e09f94bcf892fdaff09bcda1ffcbd
hachisnail <jeffersontalagtag06@gmail.com>
2025-08-04 23:41:57 +0800

    small oopsie

--- server/index.js
+++ server/index.js
@@ -48,10 +48,11 @@ app.use(session({
   cookie: {
     maxAge: 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production" ? "true": "",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: process.env.NODE_ENV === "production", 
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
   },
 }));
+
 
 // ✅ Serve static uploads only
 app.use("/uploads", (req, res, next) => {

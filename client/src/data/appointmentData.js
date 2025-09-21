91a007b48901e717c5dbc4239be2ba8e8e34fd76
rhettrina <michael18ricafrente@gmail.com>
2025-09-21 23:47:50 +0800

    temporary data

---
+++ client/src/data/appointmentData.js
@@ -0,0 +1,62 @@
+// Dummy data for appointment rates - temporary data
+export const appointmentRateData = [
+  { month: 'Jan', appointments: 28 },
+  { month: 'Feb', appointments: 22 },
+  { month: 'Mar', appointments: 16 },
+  { month: 'Apr', appointments: 40 },
+  { month: 'May', appointments: 100 },
+  { month: 'Jun', appointments: 35 },
+  { month: 'Jul', appointments: 15 },
+  { month: 'Aug', appointments: 30 },
+  { month: 'Sep', appointments: 20 },
+  { month: 'Oct', appointments: 5 },
+  { month: 'Nov', appointments: 15 },
+  { month: 'Dec', appointments: 28 }
+];
+
+// Website Traffic Overview data - area chart style
+export const websiteTrafficData = [
+  { day: 'MON', Home: 32, NewsEvents: 28, Catalogues: 24, About: 15 },
+  { day: 'TUE', Home: 28, NewsEvents: 25, Catalogues: 20, About: 12 },
+  { day: 'WED', Home: 30, NewsEvents: 27, Catalogues: 22, About: 14 },
+  { day: 'THU', Home: 31, NewsEvents: 29, Catalogues: 25, About: 16 },
+  { day: 'FRI', Home: 35, NewsEvents: 32, Catalogues: 28, About: 18 },
+  { day: 'SAT', Home: 38, NewsEvents: 35, Catalogues: 30, About: 20 },
+  { day: 'SUN', Home: 36, NewsEvents: 33, Catalogues: 28, About: 17 }
+];
+
+// Appointment Rate Pie Chart data - donut chart style
+export const appointmentPieData = [
+  { name: 'Museum Group Tour', value: 29, color: '#6B4F2C' },
+  { name: 'School Field Trip', value: 43, color: '#D2B48C' },
+  { name: 'Interviews', value: 29, color: '#654321' },
+  { name: 'Conservation Consultation', value: 25, color: '#8B6F47' },
+  { name: 'Photography or Media Projects', value: 19, color: '#3E2723' },
+  { name: 'Collaboration Meetings', value: 30, color: '#F2A93B' },
+  { name: 'Research Paper', value: 20, color: '#F9E0A5' }
+];
+
+// Visitor Quota data - progress bar style
+export const visitorQuotaData = {
+  current: 680,
+  total: 1000,
+  percentage: 68
+};
+
+// Schedules Today data - list style
+export const schedulesTodayData = [
+  { type: 'Appointment', time: '8:00am', date: '2/23/25' },
+  { type: 'Donation', time: '9:00am', date: '2/23/25' },
+  { type: 'Appointment', time: '10:00am', date: '2/23/25' },
+  { type: 'Appointment', time: '11:00am', date: '2/23/25' },
+  { type: 'Appointment', time: '2:00pm', date: '2/23/25' }
+];
+
+// Unread Queries data - list style
+export const unreadQueriesData = [
+  { type: 'Appointment', time: '8:00am', date: '2/23/25' },
+  { type: 'Donation', time: '9:00am', date: '2/23/25' },
+  { type: 'Appointment', time: '10:00am', date: '2/23/25' },
+  { type: 'Appointment', time: '11:00am', date: '2/23/25' },
+  { type: 'Appointment', time: '2:00pm', date: '2/23/25' }
+];
